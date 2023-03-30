fafda363a27813979fcb4e922d4eefbe7f3da4f5
404-WasFound <malachi.mcenroe@gmail.com>
2023-03-30 16:06:32 +1300

    started making custom structs and methods

--- src/milk.c
+++ src/milk.c
@@ -1,6 +1,48 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 
+const signed int INIT_KEYBOARD = 1;
+const signed int INIT_MOUSE = 1;
+const signed int INIT_JOYSTICK = 1;
+
+const signed int SCALE_INNER_DISPLAY = 1;
+const signed int FIT_INNER_DISPLAY = 2;
+
+typedef struct EVENT_QUEUE
+{
+
+    ALLEGRO_EVENT_QUEUE* al_event_queue;
+
+} EVENT_QUEUE;
+
+typedef struct DISPLAY
+{
+
+    ALLEGRO_DISPLAY* al_display;
+
+} DISPLAY;
+
+typedef struct EVENT_SOURCE
+{
+
+    ALLEGRO_EVENT_SOURCE* al_event_source;
+
+} EVENT_SOURCE;
+
+typedef struct TIMER
+{
+
+    ALLEGRO_TIMER* al_timer;
+
+} TIMER;
+
+typedef struct IMAGE
+{
+
+    ALLEGRO_BITMAP* bitmap;
+
+} IMAGE;
+
 void init()
 {
 
@@ -9,38 +51,59 @@ void init()
 
 }
 
-void init_event()
+EVENT_QUEUE* init_event_queue()
 {
 
-    
+    EVENT_QUEUE* event_queue;
+    event_queue->al_event_queue = al_create_event_queue();
+
+    return event_queue;
 
 }
 
-void set_fps()
+void add_event(EVENT_QUEUE* event_queue_, EVENT_SOURCE* event_source_)
 {
 
+    al_add_event_source(event_queue_->al_event_queue, event_source_->al_event_source);
 
+}
+
+DISPLAY* create_display(int width_, int height_, int mode)
+{
+
+    DISPLAY* display;
+    display->al_display = al_create_display(width_, height_);
+
+    return display;
+
+}
+
+TIMER* create_fps_timer(const signed int fps_)
+{
+
+    TIMER* timer;
+    timer->al_timer = al_create_timer(1.0 / fps_);
 
 }
 
 void init_devices(int devices[3])
 {
 
-    if (devices[0] == 1)
+    if (devices[0] != NULL)
     {
 
         al_install_keyboard();
 
     }
 
-    if (devices[1] == 1)
+    if (devices[1] != NULL)
     {
 
         al_install_mouse();
 
     }
 
-    if (devices[2] == 1)
+    if (devices[2] != NULL)
     {
 
         al_install_joystick();
